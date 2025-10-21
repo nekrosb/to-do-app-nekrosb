c6888ed99c23d2b6485a23616020a1a4ff982a59
nekrosb <makarov.nikita.mn@gmail.com>
2025-10-21 22:47:15 +0200

    feax: i feaxed all problems

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,47 @@
-import "./style.css";
+import './style.css'
 
-console.log("Hello from typescript");
+console.log('Hello from typescript')
 
-const listTodo = document.querySelector("#todo-list") as HTMLDivElement;
-const menuCreat = document.querySelector("#creat-todo") as HTMLDivElement;
+const listTodo = document.querySelector('#todo-list') as HTMLDivElement
+const menuCreat = document.querySelector('#creat-todo') as HTMLDivElement
 const addNewTodoBtn = document.querySelector(
-    "#add-new-todo-btn",
-) as HTMLButtonElement;
-const titelInput = document.querySelector("#todo-input") as HTMLInputElement;
-const plusBtn = document.querySelector(".add-btn") as HTMLButtonElement;
-menuCreat.classList.add("hidden");
+  '#add-new-todo-btn',
+) as HTMLButtonElement
+const titelInput = document.querySelector('#todo-input') as HTMLInputElement
+const plusBtn = document.querySelector('.add-btn') as HTMLButtonElement
+menuCreat.classList.add('hidden')
 
 const hiddenMainMenu = (): void => {
-    plusBtn.classList.add("hidden");
-    listTodo.classList.add("hidden");
-    menuCreat.classList.remove("hidden");
-};
+  plusBtn.classList.add('hidden')
+  listTodo.classList.add('hidden')
+  menuCreat.classList.remove('hidden')
+}
 
 const creatNewToDo = (): void => {
-    if (titelInput.value !== "") {
-    const newDiv = document.createElement("div") as HTMLDivElement;
-    newDiv.classList.add("todo");
-    const p = document.createElement("p") as HTMLParagraphElement;
-    p.textContent = titelInput.value;
-    newDiv.appendChild(p);
-    listTodo.appendChild(newDiv);
-    titelInput.value = "";
-        plusBtn.classList.remove("hidden");
-    listTodo.classList.remove("hidden");
-    menuCreat.classList.add("hidden");
-    }
-    else {
-        alert("you forget sam sings")
-    }
-};
+  if (titelInput.value !== '') {
+    const newDiv = document.createElement('div') as HTMLDivElement
+    newDiv.classList.add('todo')
+    const p = document.createElement('p') as HTMLParagraphElement
+    p.textContent = titelInput.value
+    newDiv.appendChild(p)
+    listTodo.appendChild(newDiv)
+    titelInput.value = ''
+    plusBtn.classList.remove('hidden')
+    listTodo.classList.remove('hidden')
+    menuCreat.classList.add('hidden')
+  } else {
+    alert('you forget sam sings')
+  }
+}
 
-plusBtn.addEventListener("click", hiddenMainMenu);
+plusBtn.addEventListener('click', hiddenMainMenu)
 
-addNewTodoBtn.addEventListener("click", () => {
-    creatNewToDo();
-});
+addNewTodoBtn.addEventListener('click', () => {
+  creatNewToDo()
+})
 
-titelInput.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-        creatNewToDo()
-    }
+titelInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    creatNewToDo()
+  }
 })
